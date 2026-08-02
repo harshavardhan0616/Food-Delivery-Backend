4ff8ef180f78e1b6004696c660a1bfd70e51f2f2
Harsha <pulagamharsha51@gmail.com>
2026-08-02 23:41:08 +0530

    Fix model import case

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const User = require("./models/User");
-const Order = require("./models/Order");
+const User = require("./models/user");
+const Order = require("./models/order");
 require("dotenv").config();
 
 const app = express();
